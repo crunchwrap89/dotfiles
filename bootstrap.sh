310389e5a0605d1a5c832b987b6c925ae6c9f38e
Marcus Nilsson <marcus.nilsson2@astrazeneca.com>
2024-01-20 22:07:59 +0100

    replace bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,23 +13,14 @@ mkdir ~/.local/bin
 ln -s ~/.local/squashfs-root/AppRun ~/.local/bin/nvim
 
 # Bring in our custom neovim config
-echo "XDG_CONFIG_HOME=$HOME" >> ~/.profile
-git clone https://github.com/shea-parkes/neovim-config ~/.config/nvim
+echo "XDG_CONFIG_HOME=$HOME" >>~/.profile
+npx degit https://github.com/crunchwrap89/dotfiles ~/
 cd ~/.config/nvim
-git submodule init
-git submodule update
 
-# Get Neovim mostly ready to go
 cd /workspaces/$RepositoryName
 # poetry run pip install pynvim ipython  # Avoid if we can help it
-poetry run nvim --headless +":UpdateRemotePlugins" +"q!"
-
-# Go ahead and configure vim as well while we're at it
-git clone https://github.com/shea-parkes/vim-config ~/.vim
-cd ~/.vim
-git submodule init
-git submodule update
+nvim --headless +":UpdateRemotePlugins" +"q!"
 
 # Setup git completions for bash
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
-echo "source ~/.git-completion.bash" >> ~/.bashrc
+echo "source ~/.git-completion.bash" >>~/.bashrc
