db6a10c9ed5c4199fdfd6087547d2c7cbcc944c4
Marcus Nilsson <marcus.nilsson2@astrazeneca.com>
2024-01-21 21:20:14 +0100

    some more

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-cp .tmux.conf ~
-cp .gitconfig ~
-
 # ~Manually install NeoVim
+echo "install neovim"
 mkdir -p ~/.local
 cd ~/.local
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
@@ -12,7 +10,13 @@ chmod u+x nvim.appimage
 mkdir ~/.local/bin
 ln -s ~/.local/squashfs-root/AppRun ~/.local/bin/nvim
 
+echo "install ripgrep"
+apt-get install ripgrep
+
+echo "switch to current LTS node"
 nvm use v20.11.0
+
+echo "bring in neovim and tmux config"
 # Bring in our custom neovim config
 echo "XDG_CONFIG_HOME=$HOME" >>~/.profile
 mkdir ~/dotfiles
@@ -21,10 +25,13 @@ mv ~/dotfiles/.config/nvim ~/.config/nvim
 mv ~/dotfiles/.tmux.conf ~/.tmux.conf
 
 cd /workspaces/$RepositoryName
-yarn install
-# poetry run pip install pynvim ipython  # Avoid if we can help it
+echo "update plugins"
 nvim --headless +":UpdateRemotePlugins" +"q!"
+echo "add LSP for vue"
+nvim --headless +":MasonInstall vue-language-server" +"q!"
+echo "add treesitter for vue"
+nvim --headless +":TSInstall vue" +"q!"
 
-# Setup git completions for bash
+echo "get git completions"
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
 echo "source ~/.git-completion.bash" >>~/.bashrc
